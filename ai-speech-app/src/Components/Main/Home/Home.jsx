fc9997184808cc30f6f212658d8d01d30dde1342
ilieBeracha <ilieberacha123@gmail.com>
2023-02-28 10:38:07 +0200

    a bit responsive, open a menu button under 1000 px, language valdiation

--- ai-speech-app/src/Components/Main/Home/Home.jsx
+++ ai-speech-app/src/Components/Main/Home/Home.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import Rooms from './Rooms/Rooms'
 import './Home.css'
 import SpeechFromText from './SpeakToText/SpeachFromText'
+import RoomsSmallScreen from './RoomsSmallScreen/RoomsSmallScreen';
 
 export default function Home() {
+  const smallScreen = window.matchMedia("(max-width: 1000px)").matches;
+
   return (
     <div className='Home'>
-        <Rooms />
-        <SpeechFromText />
+      {
+        smallScreen ? 
+        <RoomsSmallScreen />
+          :
+          <Rooms />
+      }
+      <SpeechFromText />
     </div>
   )
 }
---
+++ ai-speech-app/src/Components/Main/Home/RoomsSmallScreen/RoomsSmallScreen.jsx
@@ -0,0 +1,113 @@
+import React, { useState,useEffect } from 'react';
+import LogoutIcon from "@mui/icons-material/Logout";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import { useDispatch, useSelector } from "react-redux";
+import { loginRedux, logoutRedux } from "../../../../app/authSlice";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import AddCircleOutlinedIcon from "@mui/icons-material/AddCircleOutlined";
+import { apiService } from "../../../../Service/ApiService";
+import { changeRoomId, changeRoomName } from "../../../../app/roomSlice";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import DeleteIcon from '@mui/icons-material/Delete';
+import MenuIcon from '@mui/icons-material/Menu';
+import './RoomsSmallScreen.css'
+
+export default function RoomsSmallScreen() {
+    const [isOpen, setIsOpen] = useState();
+    const dispatch = useDispatch();
+    const [rooms, setRooms] = useState([]);
+    const roomSlice = useSelector((state) => state.room);
+    const [selectedRoomId, setSelectedRoomId] = useState(null);
+  
+    useEffect(() => {
+      apiService.getRoomsByUserId().then(async (res) => {
+        setRooms(res);
+      });
+    }, [roomSlice]);
+  
+    function logOut() {
+      dispatch(logoutRedux());
+    }
+  
+    async function addRoom() {
+      const newRoom = await apiService.addRoom();
+      dispatch(changeRoomId(newRoom.insertId));
+      setRooms((rooms) => [newRoom, ...rooms]);
+    }
+  
+    function handleRoomClick(roomId) {
+      setSelectedRoomId(roomId);
+      dispatch(changeRoomId(roomId));
+    }
+  
+    function handleDeleteRoom(roomId) {
+      apiService.deleteRoom(roomId).then(() => {
+        setRooms(rooms => rooms.filter(room => room.id !== roomId));
+        dispatch(changeRoomId(0))
+        dispatch(changeRoomName(''))
+      });
+    }
+  
+    async function changeLanguage(language) {
+      if (language === "") return;
+      try {
+        const results = await apiService.changeUserLanguage(language);
+        dispatch(loginRedux(results));
+        dispatch(changeRoomId(0))
+        dispatch(changeRoomName(''));
+        apiService.getRoomsByUserId().then(async (res) => {
+          setRooms(res);
+        });
+        setSelectedRoomId(null)
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    return (
+        <div className='RoomsSmallScreen'>
+            <div className='RoomsSmallScreenOpen'>
+                <div className='openMenuButton' onClick={() => setIsOpen(!isOpen)}><MenuIcon /></div>
+            </div>
+            {
+                isOpen ?
+                    <div className='Rooms'>
+                        <div onClick={addRoom} className="AddBtnDiv">
+                            <AddCircleOutlineOutlinedIcon className="hover" />
+                            <AddCircleOutlinedIcon className="not_hover" />
+                        </div>
+
+                        <div className="RoomsDiv">
+                            {rooms.map((room, index) => (
+                                <div
+                                    key={index}
+                                    className={`room ${room.id === selectedRoomId ? "selected" : ""}`}
+                                    onClick={() => handleRoomClick(room.id)}
+                                >
+                                    <div className="room_icons">
+                                        <ChatBubbleOutlineIcon className="chat_bubble_outlike_icon" />
+                                        <DeleteIcon className="delete_room_icon" onClick={() => handleDeleteRoom(room.id)} />
+                                    </div>
+                                    {room.name ?? "New Chat"}
+                                </div>
+                            ))}
+                        </div>
+                        <div className="changeLanguageDiv">
+                            <select onChange={(e) => changeLanguage(e.target.value)}>
+                                <option value="">Change Language</option>
+                                <option value="he">Hebrew</option>
+                                <option value="en">English</option>
+                                <option value="fr">Français</option>
+                                <option value="es">español</option>
+                            </select>
+                        </div>
+                        <div onClick={logOut} className="LogoutBtnDiv">
+                            <LogoutOutlinedIcon className="hover" />
+                            <LogoutIcon className="not_hover" />
+                        </div>
+                    </div>
+                    : <></>
+            }
+        </div>
+    )
+}
