73fee7e33d758665c7d35505b4142d6ab007ef9d
harelsarag7 <112413505+harelsarag7@users.noreply.github.com>
2023-03-03 02:24:00 +0200

    full screen videos

--- ai-speech-app/src/Components/Main/Videos/VideoHebrew/VideoHebrew.jsx
+++ ai-speech-app/src/Components/Main/Videos/VideoHebrew/VideoHebrew.jsx
@@ -3,7 +3,7 @@ import "./VideoHebrew.css";
 function VideoHebrew() {
     return (
         <div className="VideoHebrew">
-            <iframe className="video" width="728" height="409.5" src="https://www.youtube.com/embed/Lvi0-1h_wsc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div ><iframe style={{ borderRadius : "20px"}} width="728" height="409.5" src="https://streamable.com/e/tt82sl" frameborder="0" allowFullScreen={true} allow="autoplay"></iframe></div>
         </div>
     );
 }
